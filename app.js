36af7934bb8e514b3f294b28b2a68116c51388ab
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2020-08-28 13:59:27 -0500

    Challenge 1-10

--- app.js
+++ app.js
@@ -13,11 +13,38 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
- res.render("home");
+app.get("/", function (req, res) {
+  res.render("home", {
+    startContent: homeStartingContent
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  let post = {
+    title: req.body.title,
+    blog: req.body.blog
+  };
+
+  console.log(post);
 });
 
 
@@ -28,9 +55,6 @@ app.get("/", function(req, res){
 
 
 
-
-
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Server started on port: ${port}`);
 });
