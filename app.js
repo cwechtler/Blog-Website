9e62ba3ea275c5ff67f50722829ce251d05090b8
Craig Wechtler <30483550+cwechtler@users.noreply.github.com>
2020-09-19 01:53:30 -0500

    Add database functionality

--- app.js
+++ app.js
@@ -3,25 +3,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const loDash = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
+
+const homeStartingContent = "Have a crazy experiance? \n Have a love for something? \n Here is the place to write about it. Post your story for everyone to read and maybe they will share a story or two also. \n \n Click the COMPOSE button above and let you mind go crazy!!";
+const aboutContent = "Here at the Daily Journey we love to hear your stories. Life experiances are what made us what we are today.";
+const contactContent = "Support Team \n 555-123-4567 \n support@thiswillnotwork.not";
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = {
+  title: { type: String, required: [true, "Title is required"] },
+  content: { type: String, minlength: [10, "Post must be atleast 10 characters"] }
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    startContent: homeStartingContent,
-    postsContent: posts
+  Post.find({}, function (err, postsFound) {
+    if (!err) {
+      res.render("home", {
+        startContent: homeStartingContent,
+        postsContent: postsFound
+      });
+    }
   });
 });
 
@@ -38,34 +51,51 @@ app.get("/contact", function (req, res) {
 });
 
 app.get("/compose", function (req, res) {
-  res.render("compose");
+  res.render("compose",{
+    placeHolderMessage: "Enter Title", 
+    placeHolderPost: "Enter post with atleast 10 characters",
+    savedPost: ""
+  });
 });
 
-app.get("/post/:postName", function (req, res) {
-  const postName = loDash.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const postTitle = loDash.lowerCase(post.title);
-
-    if (postName === postTitle) {
+app.get("/post/:postId", function (req, res) {
+  const postId = req.params.postId;
+  Post.findOne({ _id: postId}, function(err, foundPost){
+    if(!err){
       res.render("post", {
-        postTitle: post.title,
-        postBlog: post.blog
-      });
+        postTitle: foundPost.title,
+        postBlog: foundPost.content
+      });     
     }
-
   });
-
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.title,
-    blog: req.body.blog
-  };
+  const writenPost = req.body.blog;
 
-  posts.push(post);
-  res.redirect("/");
+  const post = new Post({ 
+    title: req.body.title, 
+    content: writenPost
+  });
+
+  post.save(function (err) {
+    if (err) {
+      if(err.message === "Post validation failed: title: Title is required" || 
+         err.message === "Post validation failed: content: Post must be atleast 10 characters"){
+          res.render("compose", {
+            placeHolderMessage: "Title is required",
+            placeHolderPost: "Post must be atleast 10 characters long",
+            savedPost: writenPost
+          });
+      }
+      else{
+        console.log(err);
+      }
+    }
+    else{
+      res.redirect("/");
+    }
+  });
 });
 
 app.listen(port, function () {
